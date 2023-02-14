44179e434eeed8a13142425f9f00749ac368da4b
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-02-14 17:34:39 +0400

    add user registration web

---
+++ mysite/account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from account.views import registration_view, logout_view, login_view,account_view
+
+app_name = 'account'
+
+urlpatterns = [
+    path('register/', registration_view, name='user-register'),
+    path('logout/', logout_view, name = 'user-logout'),
+    path('login/', login_view, name='login'),
+    path('account/', account_view, name='account'),
+]
