6cd23e1289257dacbe2f83ba038906a881392856
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-02-17 16:56:49 +0400

    add createblogpostform view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,23 +2,25 @@ from django.shortcuts import render, redirect
 from blog.forms import CreateBlogPostForm
 from blog.models import BlogPost
 from account.models import Account
+from django.views.generic import CreateView
 
 # Create your views here.
 
 def create_blog_view(request):
-    context = {}
 
+    context = {}
     user = request.user
+
     if not user.is_authenticated:
         return redirect('account:must_authenticate')
 
     form = CreateBlogPostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         obj = form.save(commit=False)
-        author = Account.objects.filter(email = user.email).first()
+        author = Account.objects.filter(email=user.email).first()
         obj.author = author
         obj.save()
         form = CreateBlogPostForm()
 
-    context['form'] =form
+    context['form'] = form
     return render(request, 'blog/create_blog.html', context)
